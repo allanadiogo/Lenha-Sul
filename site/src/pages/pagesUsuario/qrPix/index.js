9806fa06c9952d097f2d942e549bc8da921dc4a9
SucoDfruta <noteebiel123@gmail.com>
2022-10-05 18:06:27 -0300

    mudei1

--- site/src/pages/pagesUsuario/qrPix/index.js
+++ site/src/pages/pagesUsuario/qrPix/index.js
@@ -2,6 +2,28 @@ import './index.scss';
 
 export default function Index(){
     return(
-        <h1>ai</h1>
+        <main className='page-toda'>
+            <section className='menu-amarelo'>
+            <div className='logo-menuUsuario'>
+                <img src='/assets/images/logodomenu.png'/>
+
+            </div>
+
+            <div className='itens-menu' > 
+
+            <div className='casa-menu'> 
+            <img src='/assets/images/casa-menu.png'/> Home 
+          </div>
+
+          <div className='boneco-menu'> 
+            <img src='/assets/images/bonecoperfil-menu.png'/>
+            </div>
+                 
+            </div>
+            
+           
+          
+            </section>
+        </main>
     )
 }
