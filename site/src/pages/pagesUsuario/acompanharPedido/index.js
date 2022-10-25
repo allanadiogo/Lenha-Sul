9be2d8f156a082ee16f62674ef42cc122f38a99f
sucoDfruta <noteebiel123@gmail.com>
2022-10-25 18:13:09 -0300

    .

--- site/src/pages/pagesUsuario/acompanharPedido/index.js
+++ site/src/pages/pagesUsuario/acompanharPedido/index.js
@@ -1,7 +1,23 @@
 import './index.scss';
+import  MenuAmarelo from '../../../components/menuqrpix'
 
 export default function Index(){
     return(
-        <h1></h1>
+       <main className=' menu-todinho'>
+         <MenuAmarelo/> 
+
+         <section className='section-acompanhar'> 
+
+         
+        <h1 className='h1-acompanhar'> Acompanhar pedido: </h1>
+        
+        <div>
+
+        </div>
+
+        </section>
+
+        </main>
     )
+
 }
